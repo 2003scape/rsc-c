2ebf38957119acf1811e0fb94aa51c0ebad6b105
nia <nia@NetBSD.org>
2024-03-04 11:37:46 +0100

    Do not attempt to draw mob sprites that haven't loaded
    Closes #138

--- src/mudclient.c
+++ src/mudclient.c
@@ -4583,6 +4583,11 @@ void mudclient_draw_player(mudclient *mud, int x, int y, int width, int height,
 
         if (i2 != 5 || game_data.animations[animation_id].has_a == 1) {
             int sprite_id = j5 + game_data.animations[animation_id].file_id;
+            if (mud->surface->sprite_palette[sprite_id] == NULL &&
+                mud->surface->sprite_colours[sprite_id] == NULL) {
+                /* sprite file was not loaded, probably on f2p version */
+                continue;
+            }
 
             offset_x =
                 (offset_x * width) / mud->surface->sprite_width_full[sprite_id];
@@ -4736,6 +4741,11 @@ void mudclient_draw_npc(mudclient *mud, int x, int y, int width, int height,
 
         if (i2 != 5 || game_data.animations[animation_id].has_a == 1) {
             int sprite_id = k4 + game_data.animations[animation_id].file_id;
+            if (mud->surface->sprite_palette[sprite_id] == NULL &&
+                mud->surface->sprite_colours[sprite_id] == NULL) {
+                /* sprite file was not loaded, probably on f2p version */
+                continue;
+            }
 
             offset_x =
                 (offset_x * width) / mud->surface->sprite_width_full[sprite_id];
