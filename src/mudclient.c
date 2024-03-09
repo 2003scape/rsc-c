efc946782fb85ecd8e3de473369dd71c2f93d5f5
nia <nia@NetBSD.org>
2024-03-09 15:36:30 +0100

    Restore special handling of keypad keys

--- src/mudclient.c
+++ src/mudclient.c
@@ -358,6 +358,15 @@ void get_sdl_keycodes(SDL_keysym *keysym, char *char_code, int *code) {
     *code = -1;
     *char_code = -1;
 
+    /* note: unicode is not set for key released */
+    if (keysym->unicode > 0 && keysym->unicode < 128) {
+        if (isprint((unsigned char)keysym->unicode)) {
+            *code = keysym->unicode;
+            *char_code = keysym->unicode;
+            return;
+        }
+    }
+
     switch (keysym->sym) {
     case SDLK_TAB:
         *code = K_TAB;
@@ -398,13 +407,30 @@ void get_sdl_keycodes(SDL_keysym *keysym, char *char_code, int *code) {
         *code = K_ENTER;
         *char_code = '\r';
         break;
-    default:
-        if (keysym->unicode > 0 && keysym->unicode < 128) {
-            if (isprint((unsigned char)keysym->unicode)) {
-                *code = keysym->unicode;
-                *char_code = keysym->unicode;
-            }
-        }
+    case SDLK_KP1:
+    case SDLK_1:
+        *code = K_1;
+        *char_code = K_1;
+        break;
+    case SDLK_KP2:
+    case SDLK_2:
+        *code = K_2;
+        *char_code = K_2;
+        break;
+    case SDLK_KP3:
+    case SDLK_3:
+        *code = K_3;
+        *char_code = K_3;
+        break;
+    case SDLK_KP4:
+    case SDLK_4:
+        *code = K_4;
+        *char_code = K_4;
+        break;
+    case SDLK_KP5:
+    case SDLK_5:
+        *code = K_5;
+        *char_code = K_5;
         break;
     }
 }
