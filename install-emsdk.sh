13629dd8d5294c272b6d9420bfd5465117eb4182
ipkpjersi <ipkpjersi@gmail.com>
2024-05-08 00:11:15 -0400

    Updated emsdk install script

--- install-emsdk.sh
+++ install-emsdk.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #This script probably works! If not, you might need to manually install it, likely following the instructions found here: https://emscripten.org/docs/getting_started/downloads.html#installation-instructions-using-the-emsdk-recommended
+#With that said, we currently need emsdk 3.1.22 in order for "Save and close" to not throw SDL2 audio errors.
 cd ~
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
-./emsdk install latest
-./emsdk activate latest
+./emsdk install 3.1.22
+./emsdk activate 3.1.22
 source ./emsdk_env.sh
