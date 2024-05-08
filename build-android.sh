1c85bb948aa8bd7643649ddb691e989aca28507b
ipkpjersi <ipkpjersi@gmail.com>
2024-05-07 20:07:38 -0400

    Added android and web build scripts
    These seem to work for me. I figure it's better having these than not, just in case someone else finds them useful too.

---
+++ build-android.sh
@@ -0,0 +1,4 @@
+export ANDROID_HOME="/home/$USER/android/"
+export ANDROID_NDK_HOME="/home/$USER/android/android-ndk-r24"
+cd org.scape2003.mudclient/
+./gradlew assembleDebug
---
+++ build-web.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+source ~/emsdk/emsdk_env.sh
+make -f Makefile.emscripten
+#Now, you should be able to run it with the following:
+# python3 -m http.server 1337 # @ http://localhost:1337/mudclient.html
