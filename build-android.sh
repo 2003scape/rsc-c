a95525de0bad5b95225faa6f98b5d9abe3d1976e
nia <nia@NetBSD.org>
2024-10-16 10:50:47 +0200

    Android build fixes

--- build-android.sh
+++ build-android.sh
@@ -1,8 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 #First, you should probably install Android SDK and Android NDK. 
 #You can likely install Android files with apt: sudo apt install android-sdk-build-tools android-sdk 
 #You may have to manually download Android commandline-tools and Android NDK and extract them.
-export ANDROID_HOME="/home/$USER/android/"
-export ANDROID_NDK_HOME="/home/$USER/android/android-ndk-r24"
+if [ -e /usr/lib/android-sdk ]; then
+	export ANDROID_HOME=/usr/lib/android-sdk
+	if ! [ -e /usr/lib/android-sdk/licenses/google-sdk-license ]; ten
+		git clone https://github.com/Shadowstyler/android-sdk-licenses.git
+		sudo cp -p android-sdk-licenses/*-license /usr/lib/android-sdk/licenses
+	fi
+else
+	export ANDROID_HOME="/home/$USER/android/"
+	export ANDROID_NDK_HOME="/home/$USER/android/android-ndk-r24"
+fi
 cd org.scape2003.mudclient/
 ./gradlew assembleDebug
